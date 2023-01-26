852bf2c534d5ef60105624a57f71ff6c840517e0
artorias961 <artorias961@gmail.com>
2023-01-26 15:52:31 -0800

    adding files

---
+++ code/main.py
@@ -0,0 +1,12 @@
+import cv2
+
+
+
+class DetectContours():
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def get_image(self):
+        #
+
+if __name__ == '__main__':
